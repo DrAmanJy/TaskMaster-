88735a04500ad9361c8daeefa7dd3c587ec61eda
DrAmanJy <dramanjy0770@gmail.com>
2026-03-21 19:43:59 +0530

    refactor: update main application and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import LoginPage from "./components/LoginPage";
-import SignUpPage from "./components/SignUpPage";
-
 export default function App() {
-  return <div className=" text-green-700 bg-white">lund</div>;
+  return <div className="">lund</div>;
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Auth from "./pages/Auth";
+import SigninPage from "./pages/SigninPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import DashboardPage from "@/pages/DashboardPage.jsx";
 
 const routes = createBrowserRouter([
   { index: true, element: <App /> },
-  { path: "/auth", element: <Auth /> },
+  { path: "/signin", element: <SigninPage /> },
+  { path: "/signup", element: <SignupPage /> },
+  { path: "/dashboard", element: <DashboardPage /> },
 ]);
 export default routes;
